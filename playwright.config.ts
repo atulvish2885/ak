18d68b6dd38dc2a22bccf291dd3c4056e5345950
Atul Vish <atulvishwakarma725@gmail.com>
2025-03-17 16:50:54 +0530

    commit 17march

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,16 +38,21 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: {
+    //     ...devices['iPhone 13']},
+    //   }
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/FirstTest.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Makemytrip Successfully',()=>{
+
+    test('makemytrip', async ({ page }) => {
+        await page.goto('https://www.makemytrip.com/');
+        await page.waitForLoadState('networkidle') 
+        const locator=page.locator("//div[@class='style__LoginRegularSaverWrapper-sc-1ioaggt-0 ugrWM']");
+        await expect(locator).toBeVisible();
+        
+        // Expect a title "to contain" a substring.
+        //await expect(page).toHaveTitle(/Playwright/);
+      });
+
+      test.only('Linkedin Automation', async ({ page }) => {
+        await page.goto('https://www.linkedin.com/');
+        await page.locator("//span[normalize-space()='People']").click();
+        await expect(page.getByText('Try searching for your co-worker, classmate, professor, or friend.')).toBeVisible();
+      
+      });
+})

5e7cc0a3825d95cb00295c11a848ac40a418d4a9
Atul Vish <atulvishwakarma725@gmail.com>
2026-02-06 11:05:49 +0530

    NN

--- tests/UI/TestAutomation.spec.ts
+++ tests/UI/TestAutomation.spec.ts
@@ -1,11 +1,19 @@
 import { test, expect } from '@playwright/test';
+const baseurl= 'htps://flipkart.com';
 
-test.describe('Travel',()=>{
 
-    test('reworked', async ({ page }) => {
-        await page.goto('https://devapp.reworked.ai/');
+test.describe('flipkart',()=>{
+
+    test('Flipkart Automation', async ({ page }) => {
+
+      const inputbox = await page.locator('//input[@class="lNPl8b"]');
+        await page.goto(baseurl);
         
-        // Expect a title "to contain" a substring.
-        //await expect(page).toHaveTitle(/Playwright/);
+        await inputbox.fill("Apple Iphone 17 PRO");
+        await inputbox.press('Enter');
+        await page.waitForTimeout(3000);
+        
+
+
       });
 })
